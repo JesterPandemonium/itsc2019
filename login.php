ca1381fa057a9b9321591ff140f701f346e6f090
Peter <53820878+weilichsoheisse@users.noreply.github.com>
2019-08-06 13:35:41 +0200

    Update login.php
    The main part of the login page

--- login.php
+++ login.php
@@ -11,7 +11,42 @@
 </head>
 <body>
     <div id="header"></div>
-    <div id="main"></div>
+    <div id="main">
+    <div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Anmelden</h3>
+			</div>
+			<div class="card-body">
+				<form>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="email" class="form-control" placeholder="Email">
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" placeholder="Passwort">
+					</div>
+					<div class="form-group center-block">
+						<input type="submit" value="Login" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center">
+					<a href="#">Passwort vergessen?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+    </div>
     <div id="footer"></div>
 </body>
 </html>
