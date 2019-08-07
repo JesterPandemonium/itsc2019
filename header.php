e64aeaa5b8e6a0ec7f759db0d4e72857ff76ba6b
autoramme <53813829+autoramme@users.noreply.github.com>
2019-08-07 10:15:36 +0200

    header

---
+++ header.php
@@ -0,0 +1,36 @@
+<html lang="en">
+    
+    <link rel="stylesheet" href="styles/start.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+<header>
+    
+ <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">events</a>
+  <a href="#contact">kontakte</a>
+  <a href="#about">text?</a>
+</div>   
+
+    
+
+    
+    
+</header>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</html>
