90b4de2ee6bd21aeeb53b2594d4ba7921f1558f9
Peter <53820878+weilichsoheisse@users.noreply.github.com>
2019-08-07 11:45:59 +0200

    update title

--- start.php
+++ start.php
@@ -3,7 +3,7 @@
 
 <html lang="en">
     <head>
-        <title>AweSplash - Free HTML Splash Page</title>
+        <title>Startseite</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
